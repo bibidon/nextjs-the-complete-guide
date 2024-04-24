26d05e74605a5599a57328c79fb573e6139d1fe0
Denis Lutsevich <debibidon@gmail.com>
2024-04-24 14:27:01 +0300

    Add meals, shared meal, details meal, community and main header components, complete main header component.

---
+++ app/community/page.js
@@ -0,0 +1,5 @@
+export default function Community() {
+    return (
+        <h1>Community component</h1>
+    );
+}
---
+++ app/meals/share/page.js
@@ -0,0 +1,5 @@
+export default function ShareMeal() {
+    return (
+        <h1>Share meal component</h1>
+    );
+}
